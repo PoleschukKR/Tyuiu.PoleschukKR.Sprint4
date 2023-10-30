4b78ec210ad79a9db664f3bb227f9abe26ff4ab5
Tyuiu.PoleschukKR@outlook.com <kirillpolesuk05@gmail.com>
2023-10-30 19:42:23 +0500

    Task1

---
+++ Tyuiu.PoleschukKR.Sprint4.Task1.V28.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.PoleschukKR.Sprint4.Task1.V28.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.PoleschukKR.Sprint4.Task1.V28.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("ecb55450-9a83-4c12-9ffd-9f4ebe91d0be")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
