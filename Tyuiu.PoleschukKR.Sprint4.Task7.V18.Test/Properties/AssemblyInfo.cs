929c68e650532f79eecd00c4189f2b668074d3e6
Tyuiu.PoleschukKR@outlook.com <kirillpolesuk05@gmail.com>
2023-11-07 14:59:14 +0500

    Task7

---
+++ Tyuiu.PoleschukKR.Sprint4.Task7.V18.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.PoleschukKR.Sprint4.Task7.V18.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.PoleschukKR.Sprint4.Task7.V18.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("1315abc7-6cc4-4866-888f-e6b40cdbbbe2")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.PoleschukKR.Sprint4.Task7.V18/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.PoleschukKR.Sprint4.Task7.V18.Lib;
+
+namespace Tyuiu.PoleschukKR.Sprint4.Task7.V18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #4 | Выполнил: Полещук К. Р. | ИИПб-23-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #4                                                               *");
+            Console.WriteLine("* Тема:Добавление решений итогового проекта                               *");
+            Console.WriteLine("* Задание #7                                                              *");
+            Console.WriteLine("* Вариант #18                                                             *");
+            Console.WriteLine("* Выполнил: Полещук Кирилл Романович | ИИПб-23-1                          *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Дана строка из одноразрядных цифр '145258749635789'. Преобразуйте ее в  *");
+            Console.WriteLine("*  матрицу 5 на 3 и подсчитайте произведение четных чисел.                *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            
+            string value = "145258749635789";
+            int[,] mtrx = new int[5, 3];
+            int n = 5;
+            int m = 3;
+            Console.WriteLine("Входная строка: " + value);
+            Console.WriteLine("Получившаяся матрица: ");
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    mtrx[i, j] = int.Parse(value.Substring(i * m + j, 1));
+                    Console.Write(mtrx[i,j] + "\t");
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Произведение четных элементов массива: " + ds.Calculate(n, m, value));
+            Console.ReadKey();
+        }
+    }
+}
